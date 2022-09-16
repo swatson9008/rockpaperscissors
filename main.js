8e3b8ec80c51b72cb75da288513c95e11b766d37
Summer <swatson9008@yahoo.com>
2022-09-16 15:55:44 -0400

    wrote the pseudo code

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// Write an array of choices the computer can make
+
+
+
+// Write a function that lets the computer choose between one of three choices. Call it getComputerChoice
+
+// Write a const variable for player selection, make it case insensitive
+
+// Write a const variable for the computer selection
+
+// Write a function to compare the computer selection against the player selection. Include the rules of rock, paper, scissors inside the function
+
+// Test the function
+
+// Make the function return a string based on the results
+
+// Make a function to play a game that keeps score and plays for five rounds.
+
+// Make a prompt to get input from the player
