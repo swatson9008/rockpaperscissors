6d42e50bec792c334e3cca0cd8b8152b123f4190
Summer <swatson9008@yahoo.com>
2022-09-18 12:26:48 -0400

    passed the looping test

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function getComputerChoice () {
 
 // Write a const variable for player selection, make it case insensitive
 
-const playerSelection = "Scissors";
+const playerSelection = "Rock";
 
 const playerSelect = playerSelection.toLowerCase();
 
@@ -32,66 +32,34 @@ const computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
 
-/*function playRound (playerSelect, computerSelection, playerScore){
-
-   if (playerSelect==="rock" && computerSelection==="scissors"){
-   return playerScore + 1
-   }
-   else if (playerSelect==="rock" && computerSelection==="paper"){
-   return "Computer wins, paper beats rock."
-   }
-   else if (playerSelect==="rock" && computerSelection==="rock"){
-   return "Tie."
-   }
-   else if (playerSelect==="paper" && computerSelection==="scissors"){
-   return "Computer wins, scissors beat paper."
-   }
-   else if (playerSelect==="paper" && computerSelection==="paper"){
-   return "Tie."
-   }
-   else if (playerSelect==="paper" && computerSelection==="rock"){
-   return "Player wins, paper beats rock."
-   }
-   else if (playerSelect==="scissors" && computerSelection==="scissors"){
-   return "Tie."
-   }
-   else if (playerSelect==="scissors" && computerSelection==="paper"){
-   return "Player wins, scissors beat paper."
-   }
-   else if (playerSelect==="scissors" && computerSelection==="rock"){
-   return "Computer wins, rock beats scissors."
-   }
-    
-}*/
-
-function playRound (playerSelect, computerSelection, playerScore, computerScore){
+function playRound (playerSelect, computerSelection){
 
     if (playerSelect==="rock" && computerSelection==="scissors"){
-    playerScore = playerScore++
+    ++playerScore
     }
     else if (playerSelect==="rock" && computerSelection==="paper"){
-    computerScore = computerScore++
+    ++computerScore
     }
     else if (playerSelect==="rock" && computerSelection==="rock"){
     
     }
     else if (playerSelect==="paper" && computerSelection==="scissors"){
-    computerScore = computerScore++
+    ++computerScore
     }
     else if (playerSelect==="paper" && computerSelection==="paper"){
-    
+     
     }
     else if (playerSelect==="paper" && computerSelection==="rock"){
-    playerScore = playerScore++
+    ++playerScore
     }
     else if (playerSelect==="scissors" && computerSelection==="scissors"){
     
     }
     else if (playerSelect==="scissors" && computerSelection==="paper"){
-    playerScore = playerScore++
+    ++playerScore
     }
     else if (playerSelect==="scissors" && computerSelection==="rock"){
-    computerScore = computerScore++
+    ++computerScore
     }
      
  }
@@ -106,19 +74,13 @@ const playRoundresult = playRound(playerSelect, computerSelection);
 
 // Make a function to play a game that keeps score and plays for five rounds.
 
+function game(){
+  for (let i = 0; i < 5; i++){
+    playRound(playerSelect, computerSelection);
+    return(playerScore)
+    }
+}
 
-
-/*function game(){
-    if (playRound(playerSelect, computerSelection)=(playRoundresult.startsWith("Player")=true)){
-        return "Player wins round 1."
-    }
-    if (playRound(playerSelect, computerSelection)=(playRoundresult.startsWith("Computer")=true)){
-        return "Computer wins round 1."
-    }
-    if (playRound(playerSelect, computerSelection)=(playRoundresult.startsWith("Tie")=true)){
-        return "It's a draw."
-    }
-}*/
 
 console.log(game());
 
